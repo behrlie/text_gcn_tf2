1dbcc849085c8e2d4130b067165fadb4daaf5b8e
Marcus Behr <marcus.behr@fau.de>
2026-04-06 16:13:37 +0200

    Updated WOS train/test split to match CNN data

--- prepare_wos.py
+++ prepare_wos.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 #-*-coding:utf-8-*-
 
-from sklearn.model_selection import train_test_split
-
 with open('data/WOS11967/X.txt', 'r') as f:
     sentences = f.readlines()
 with open('data/WOS11967/Y.txt', 'r') as f:
@@ -11,8 +9,11 @@ with open('data/WOS11967/Y.txt', 'r') as f:
 sentences = [s.replace('\n', '') for s in sentences]
 labels_int = [int(l.strip()) for l in labels]
 
-train_sentences, test_sentences, train_labels, test_labels = train_test_split(
-    sentences, labels_int, test_size=0.2, random_state=42, stratify=labels_int)
+last_train_index = int(0.8 * len(sentences))
+train_sentences = sentences[:last_train_index]
+train_labels = labels_int[:last_train_index]
+test_sentences = sentences[last_train_index:]
+test_labels = labels_int[last_train_index:]
 
 mapping = {10: 'Person-perception', 1: 'Machine-learning', 29: 'Cell-biology', 16: 'Machine-design', 18: 'Ambient-Intelligence', 6: 'Electrical-circuits', 15: 'Manufacturing-engineering', 13: 'computer-aided-design', 11: 'Nonverbal-communication', 7: 'Digital-control', 28: 'Molecular-biology', 24: 'Allergies', 31: 'Immunology', 26: 'Ankylosing-Spondylitis', 22: 'Water-Pollution', 12: 'Prosocial-behavior', 17: 'Fluid-mechanics', 0: 'Computer-vision', 9: 'Social-cognition', 25: "Alzheimer's-Disease", 8: 'Prejudice', 3: 'Cryptography', 32: 'Genetics', 19: 'Geotextile', 2: 'network-security', 30: 'Human-Metabolism', 5: 'Electricity', 20: 'Remote-Sensing', 27: 'Anxiety', 21: 'Rainwater-Harvesting', 14: 'Hydraulics', 23: 'Addiction', 4: 'Operating-systems'}
 
